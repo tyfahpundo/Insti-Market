b1b35eafcb252c86da93e392ac02124f28e53a13
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-06-16 15:00:25 +0200

    Regiter User Now Working

--- login.php
+++ login.php
@@ -60,7 +60,7 @@ $password = mysqli_real_escape_string($con,$password);
 $pass_md5=md5($password);
 // check if supplied details exist
 $result=mysqli_query($con,"SELECT * FROM members WHERE user='$username' 
-AND password='$pass_md5'") or die(mysqli_error());
+AND password='$pass_md5'") or die(mysqli_error($con));
 
 // Mysql_num_row is counting table row
 $count=mysqli_num_rows($result);
@@ -121,35 +121,36 @@ if(isset($_POST['signup']))
 		
 $name=($_POST['name']);
 $uname=($_POST['uname']);
-$college=($_POST['college']);
-$email=($_POST['email']);
 $pass=($_POST['pass']);
 $pass2=($_POST['pass2']);
 $mobile=($_POST['mobile']);
+$email=($_POST['email']);
+$college=($_POST['college']);
+
 // $state=($_POST['muppetname']);
 //$lga=($_POST['lga']);
 
 $name = stripslashes($name);
 $uname = stripslashes($uname);
-$college = stripslashes($college);
-$email = stripslashes($email);
 $pass = stripslashes($pass);
 $pass2 = stripslashes($pass2);
 $mobile= stripslashes($mobile);
+$email = stripslashes($email);
+$college = stripslashes($college);
+
 // $state = stripslashes($state);
 //$lga= stripslashes($lga);
 
 $name = mysqli_real_escape_string($con,$name);
 $uname = mysqli_real_escape_string($con,$uname);
-$college = mysqli_real_escape_string($con,$college);
-$email = mysqli_real_escape_string($con,$email);
 $pass = mysqli_real_escape_string($con,$pass);
 $pass2 = mysqli_real_escape_string($con,$pass2);
 $mobile = mysqli_real_escape_string($con,$mobile);
+$email = mysqli_real_escape_string($con,$email);
+$college = mysqli_real_escape_string($con,$college);
 
 
-
-	if($name =="" || $pass=="" ||$pass2=="" || $uname =="" ||$college =="" || $email=="" || $mobile=="")
+	if($name =="" || $pass=="" ||$pass2=="" || $uname =="" || $email=="" || $mobile=="" || $college=="")
         {
 		echo "All fields must be completed please<br/>";
                 }
@@ -169,9 +170,9 @@ $mobile = mysqli_real_escape_string($con,$mobile);
         else
         {
       $pass_md5=md5($pass);
-		$insert = "INSERT INTO members (name,user,password,email,phone,college)
-VALUES ('$name','$uname','$pass_md5','$email','$mobile','$college')";
-		mysqli_query($con,$insert) or die('Cannot add record '.mysqli_error());
+		$insert = "INSERT INTO members (name,user,password,phone,email,college)
+VALUES ('$name','$uname','$pass_md5','$mobile','$email','$college')";
+		mysqli_query($con,$insert) or die('Cannot add record '.mysqli_error($con));
 		
              echo "<script type='text/javascript'>
                 window.alert(' Successful! you can now login ');
@@ -207,23 +208,27 @@ VALUES ('$name','$uname','$pass_md5','$email','$mobile','$college')";
                             <input type="email" name="email" placeholder="Your Email">
                         </div>
 
-
                         <div class="form-row">
+                            <input type="text" name="college" placeholder="college">
+                        </div>
+
+
+                        <!-- <div class="form-row">
                             <label>Institution type</label><br />
                             <select name="muppetname" onchange="changeContent(this.value)">
-                                <option> </option>
-                                <?php
+                                <option> </option> -->
+                        <!-- <?php
 
 
 
-$sql = "select * from college_cat";
+//$sql = "select * from college_cat";
 
-$result = mysqli_query($con,$sql);
+//$result = mysqli_query($con,$sql);
 
-while ($ary = mysqli_fetch_array($result)){
+//while ($ary = mysqli_fetch_array($result)){
 
-	echo "<option value=\"" . $ary['id']  . "\">" . $ary ['typ']  . "</option>";
-}
+//	echo "<option value=\"" . $ary['id']  . "\">" . $ary ['typ']  . "</option>";
+//}
 
 
 //mysql_close($link);
@@ -232,12 +237,12 @@ while ($ary = mysqli_fetch_array($result)){
 ?>
                             </select>
 
-                        </div>
+                            <!-- </div>
                         <label>Institution</label><br />
                         <select name="college" id="options2">
                             <option></option>
 
-                        </select>
+                        </select> -->
 
 
 
